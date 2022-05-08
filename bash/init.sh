c5c89371e21beea897dcbe3821111b065cdd75b0
Andrew Jayne <majora158@gmail.com>
2022-05-08 15:21:25 -0600

    added ansible and terraform

--- bash/init.sh
+++ bash/init.sh
@@ -3,12 +3,12 @@ sudo apt update
 sudo apt upgrade
 
 #install main  programs included in apt
-sudo apt install vim apt-transport-https curl zsh python-is-python3 python3-pip snapd ca-certificates curl gnupg lsb-release virtualbox  ffmpeg python3.9-venv
+sudo apt install vim apt-transport-https curl ansible zsh python-is-python3 python3-pip snapd ca-certificates curl gnupg lsb-release virtualbox termffmpeg python3.9-venv
 
 #install pip
 python3 -m ensurepip --upgrade
 
-#install Keys and GPG for Brave, Sublime Test, Docker, KubeCtl, AWS Workspaces, OBS
+#install Keys and GPG for Brave, Sublime Test, Docker, KubeCtl, AWS Workspaces, OBS, Terraform
 	#brave
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
@@ -26,18 +26,20 @@ echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https:/
 	#GCP CLI
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-	#AWS Workspaces
-wget -q -O - https://workspaces-client-linux-public-key.s3-us-west-2.amazonaws.com/ADB332E7.asc | sudo apt-key add -
-echo "deb [arch=amd64] https://d3nt0h4h6pmmc4.cloudfront.net/ubuntu bionic main" | sudo tee /etc/apt/sources.list.d/amazon-workspaces-clients.list 
+	#Terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 	#OBS
 sudo add-apt-repository ppa:obsproject/obs-studio
 
-#install Brave, Sublime Test, Docker, KubeCtl, OBS
+
+#install Brave, Sublime Test, Docker, KubeCtl, OBS, Terraform
 sudo apt update
-sudo apt install brave-browser sublime-text docker-ce docker-ce-cli containerd.io kubectl google-cloud-sdk workspacesclient obs-studio awscli
+sudo apt install brave-browser terraform sublime-text docker-ce docker-ce-cli containerd.io kubectl google-cloud-sdk workspacesclient obs-studio awscli
 
 
 #install Azure CLI
+sudo apt remove azure-cli
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 #install slack
