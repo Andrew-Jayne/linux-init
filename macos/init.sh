94464e6d466562aab4603729206693b078997091
Andrew Jayne <majora158@gmail.com>
2022-07-10 18:44:05 -0600

    added macOS

--- macos/init.sh
+++ macos/init.sh
@@ -1,1 +1,2 @@
-#init.sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install brave-browser firefox vlc signal visual-studio-code sublime-text zoomus ableton-live-lite
