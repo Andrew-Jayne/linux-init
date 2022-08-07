725a95c518c14b6b7e496e6b46b0f9856d9a5152
Poppers <poppers@pop-os.localdomain>
2022-08-07 16:03:01 -0700

    removed temp script

--- linux/bash/tf.sh
+++
@@ -1,1 +0,0 @@
-sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/terraform-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/terraform.list
