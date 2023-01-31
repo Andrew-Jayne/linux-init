8b660b49b5bf0efbb9895a6885958ac508f95d3d
Andrew Jayne <andrewjayne@pop-os.localdomain>
2023-01-30 20:57:41 -0700

    more linux stuff

--- linux/bash/cli_init.sh
+++ linux/bash/cli_init.sh
@@ -12,27 +12,26 @@ sudo chsh -s /bin/zsh
 ## Install pip
 python3 -m ensurepip --upgrade
 
-##  Install packages from external sources
+#  Install packages from external sources
 
-	#install Keys and GPG for Brave, Sublime Text, Docker, KubeCtl, Terraform, OBS, Helm
-
+	#install Keys and GPG for Docker, KubeCtl, Terraform, OBS, Helm
 		#docker
 		sudo apt-get remove docker docker-engine docker.io containerd runc
 		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 		echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 		
 		#kubectl
-		sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-		echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+		sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+		echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 		
 		#helm
 		curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
 		echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 
 		#Terraform
-		wget -qO - terraform.gpg https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/terraform-archive-keyring.gpg
-		sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/terraform-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > etc/apt/sources.list.d/terraform.list
-
+		wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+		echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+	
 	#install Docker, KubeCtl, Terraform, AWS CLI
 	sudo apt update
 	sudo apt install terraform docker-ce docker-ce-cli containerd.io docker-compose-plugin kubectl awscli helm
@@ -46,18 +45,6 @@ pip3 install git+https://github.com/ansible-community/ansible-lint.git
 
 # Add kubectl Alias
 echo 'alias kc=kubectl' > ~/.zshrc
-
-## Closing messages
-echo ''
-echo ''
-echo ''
-echo 'Relauch terminal to pick up OhMyZSH'
-echo 'Azure VDI is restricted to web only, what a sham'
-echo 'Edge DL link -> https://www.microsoftedgeinsider.com/en-us/download?platform=linux-deb'
-echo 'Enjoy the new install'
-
-
-
 
 # Clean Up .deb files
 rm *.deb
