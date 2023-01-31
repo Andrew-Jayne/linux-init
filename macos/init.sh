b7faab471fd28cefe6cadabd3fdc4015e833828c
Andrew Jayne <andrewjayne@pop-os.localdomain>
2023-01-30 20:54:52 -0700

    mac updates

--- macos/init.sh
+++ macos/init.sh
@@ -1,2 +1,7 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install brave-browser firefox vlc signal visual-studio-code sublime-text zoomus ableton-live-lite
+brew install brave-browser firefox vlc signal visual-studio-code sublime-text zoomus 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Add Encroyt & Decrypt Aliases
+alias encrypt="openssl aes-256-cbc -a -salt"
+alias decrypt="openssl aes-256-cbc -d -a"
