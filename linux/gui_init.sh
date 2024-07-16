2aac7e10d21168e1fcce88a1ef41e3bf5a7648a6
Andrew Jayne <andrew.jayne@cyberdefense.global>
2024-07-16 11:19:47 -0600

    fix formatting, remove programs, ready for 2024

--- linux/gui_init.sh
+++ linux/gui_init.sh
@@ -3,11 +3,41 @@ sudo apt update
 sudo apt upgrade -y
 
 #remove tools extra tools
-sudo apt remove nano geary firefox totem libreoffice-base-core libreoffice-common libreoffice-style-colibre libreoffice-style-yaru  gnome-calculator gnome-calendar gnome-contacts -y
+sudo apt remove -y  nano \
+										geary \ 
+										firefox \ 
+										awscli \ 
+										pop-shop \
+										libreoffice-base-core \ 
+										libreoffice-common \
+										libreoffice-style-colibre \ 
+										libreoffice-style-yaru \ 
+										gnome-calculator \ 
+										gnome-calendar \ 
+										gnome-contacts \
+										gnome-remote-desktop \
+										gnome-weather \
+										ubuntu-pro-client
+
 sudo apt autoremove -y 
 
 ## Install additional native packages
-sudo apt install vim apt-transport-https gparted curl ansible zsh python3 python-is-python3 python3-pip ca-certificates software-properties-common curl gnupg lsb-release virtualbox ffmpeg yamllint flatpak wget chromium-browser nmap brasero exfat-fuse dosfstools virt-manager -y
+sudo apt install -y vim \ 
+										apt-transport-https \
+										gparted \ 
+										curl \ 
+										zsh \ 
+										python3 \
+										python-is-python3 \
+										python3-pip \ 
+										ca-certificates \
+										software-properties-common \ 
+										gnupg \
+										lsb-release\
+										ffmpeg \
+										wget \  
+										nmap \ 
+										exfat-fuse\
 
 #Set up etc/resolve.conf for AVX VPN
 sudo dpkg-reconfigure resolvconf
@@ -17,12 +47,8 @@ python3 -m ensurepip --upgrade
 
 ##  Install packages from external sources
 
-	#install Keys and GPG for Brave Beta, Docker, KubeCtl, Terraform, OBS, Helm
-		
-		#brave beta
-		sudo curl -fsSLo /usr/share/keyrings/brave-browser-beta-archive-keyring.gpg https://brave-browser-apt-beta.s3.brave.com/brave-browser-beta-archive-keyring.gpg
-		echo "deb [signed-by=/usr/share/keyrings/brave-browser-beta-archive-keyring.gpg] https://brave-browser-apt-beta.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-beta.list
-		
+	#install Keys and GPG for, Docker, KubeCtl, Terraform, OBS, Helm
+
 		#docker
 		sudo apt remove docker docker-engine docker.io containerd runc
 		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -36,28 +62,34 @@ python3 -m ensurepip --upgrade
 		curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
 		echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 
-		#Terraform
-		wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-		echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-
-
-	#install Brave, Docker, KubeCtl, Terraform, Sublime Text, OBS
+	#install Docker, KubeCtl, Helm
 	sudo apt update
-	sudo apt install brave-browser-beta terraform docker-ce docker-ce-cli containerd.io docker-compose-plugin kubectl awscli helm -y
+	sudo apt install -y docker-ce \
+											docker-ce-cli \
+											containerd.io \
+											docker-compose-plugin \
+											kubectl \
+											helm 
 
 ## Congifure Group Permissions for Docker
 sudo groupadd docker
 sudo usermod -aG docker $USER
+
 #this keeps us from needing a logout/login to pick up the new group
 newgrp docker
 
 ## Install .deb packages
-	#install AVX VPN Client
+
+	# AVX VPN Client
 	curl "https://aviatrix-download.s3-us-west-2.amazonaws.com/AviatrixVPNClient/AVPNC_linux_JammyJellyfish.deb" -o AVPNC_linux_JammyJellyfish.deb
 	sudo dpkg -i AVPNC_linux_JammyJellyfish.deb
 
-	#install VS Code
+	# VS Code
 	curl "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -o vscode.deb
+	sudo dpkg -i vscode.deb
+	
+	# Edge
+	curl "https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_126.0.2592.102-1_amd64.deb?brand=M102" -0 edge.deb
 	sudo dpkg -i vscode.deb
 
 ## Install Azure CLI
@@ -66,30 +98,43 @@ curl -sL "https://aka.ms/InstallAzureCLIDeb" | sudo bash
 
         
  ## Install Flatpak Packages
-    #install Cider
-	flatpak install flathub sh.cider.Cider -y
-    #install Bitwarden
-	flatpak install flathub com.bitwarden.desktop -y
-    #install Remmina RDP
-	flatpak install flathub org.remmina.Remmina -y
-    #install Handbrake
-	flatpak install flathub fr.handbrake.ghb -y
-    #install Signal
-	flatpak install flathub org.signal.Signal -y
-	#install Gimp
-	flatpak install flathub org.gimp.GIMP -y
-	#install Firefox
-	flatpak install flathub org.mozilla.firefox -y
-	#install Brave Stable
-	flatpak install flathub com.brave.Browser -y
-	#install Chromimum
-	flatpak install flathub org.chromium.Chromium -y
-	#install OBS studio
-	flatpak install flathub com.obsproject.Studio -y
+
+  ## Add flathub repo
+	flatpak remote-add --if-not-exists -y flathub https://flathub.org/repo/flathub.flatpakrepo
+
+  ## Text & Diagram Editors
+	flatpak install -y md.obsidian.Obsidian \
+										 com.jgraph.drawio.desktop
+	
+	## Remote Access
+	flatpak install -y org.remmina.Remmina
+
+  ## Password Managment
+	flatpak install -y com.bitwarden.desktop
+	
+  ## Communication
+	flatpak install -y org.signal.Signal \
+										 us.zoom.Zoom 
+
+	## Web Browsers
+	flatpak install -y org.mozilla.firefox \
+										 com.brave.Browser \
+										 org.chromium.Chromium \
+										 io.gitlab.librewolf-community \
+
+	## Media Tools
+	flatpak install -y fr.handbrake.ghb \
+										 com.obsproject.Studio \
+										 org.upscayl.Upscayl \
+										 org.gimp.GIMP \
+										 sh.cider.Cider \
+										 org.videolan.VLC \
+										 org.shotcut.Shotcut \
+										 no.mifi.losslesscut \
+
 	# Extra Gnome apps
-	flatpak install flathub org.gnome.Calculator
-	flatpak install flathub org.gnome.Calendar
-	flatpak install flathub org.gnome.TextEditor
+	flatpak install -y org.gnome.Calculator \
+										 org.gnome.Calendar
 
 ## Install OhMyZsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -101,9 +146,8 @@ echo 'alias kc=kubectl' >> ~/.zshrc
 # Addo code runner aliases
 echo 'alias py=python3' >> ~/.zshrc
 echo 'alias python=python3' >> ~/.zshrc
-echo 'alias tf=terraform' >> ~/.zshrc
 
-# Add Encroyt & Decrypt Aliases
+# Add Encrypt & Decrypt Aliases
 echo 'alias encrypt=openssl aes-256-cbc -a -salt' >> ~/.zshrc
 echo 'alias decrypt=openssl aes-256-cbc -d -a' >> ~/.zshrc
 
@@ -117,7 +161,6 @@ echo ''
 echo ''
 echo ''
 echo 'Relauch terminal to pick up OhMyZSH'
-echo 'Azure VDI is restricted to web only, what a sham'
 echo 'Enjoy the new install'
 
 
@@ -131,7 +174,8 @@ sudo docker run hello-world || echo 'Docker install failed'
 python --version || echo 'Python install failed'
 aws --version || echo 'AWS CLI install failed'
 az version || echo 'Azure CLI install failed'
-terraform --version || echo 'Terraform install failed'
+gcloud --version || echo ' GCP CLI Install Failed'
 pip --version || echo 'Pip install failed'
 kubectl version --short || echo 'Kubectl install failed'
+helm --version || echo 'Helm Install Failed'
 flatpak --version || echo 'Flatpak install failed'
